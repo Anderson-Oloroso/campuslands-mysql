bc4a20191b183715e116bbaee9a6a4bff84d0510
perezcleidy069-sketch <perezcleidy069@gmail.com>
2026-07-27 00:24:11 -0600

    feat(mysql-basico):resolver ejercicio 032

---
+++ basico/ejercicio-032/resoluciones/cleidy-perez/dml/inserts.sql
@@ -0,0 +1,8 @@
+-- Datos de practica. Puedes cambiarlos o agregar mas filas.
+USE campuslands_mysql;
+
+
+INSERT INTO battle_royale (nombre, categoria, puntaje, estado) VALUES
+('Alpha 001', 'principal', 51.50, 'activo'),
+('Beta 001', 'secundario', 36.00, 'revision'),
+('Gamma 001', 'principal', 21.75, 'activo');
