8e0c53072e7994520e3dc6ccb1dc24f7dcff6c68
perezcleidy069-sketch <perezcleidy069@gmail.com>
2026-07-26 23:01:49 -0600

    feat(mysql-basico):resolver ejercicio 022

---
+++ basico/ejercicio-022/resoluciones/cleidy-perez/dml/inserts.sql
@@ -0,0 +1,9 @@
+-- Datos de practica. Puedes cambiarlos o agregar mas filas.
+USE campuslands_mysql;
+
+INSERT INTO proyectos_animacion_3d (nombre_proyecto, estudio, software_principal, duracion_segundos, fotogramas_totales, tiempo_render_horas, complejidad, fecha_entrega) VALUES
+('Mecanismo Sci-Fi', 'RenderStudio', 'Blender', 15, 360, 12.5, 'Media', '2026-04-10'),
+('Caminata de Personaje', 'PixAnim', 'Maya', 5, 120, 4.0, 'Baja', '2026-02-15'),
+('Simulación de Agua y Fuego', 'VFX Pro', 'Cinema 4D', 30, 720, 85.0, 'Extrema', '2026-08-01'),
+('Secuencia Cinematográfica', 'Virtual World', 'Unreal Engine', 60, 1440, 6.0, 'Alta', '2026-06-20'),
+('Cortometraje Robot', 'PixAnim', 'Blender', 45, 1080, 42.0, 'Alta', '2026-05-30');
