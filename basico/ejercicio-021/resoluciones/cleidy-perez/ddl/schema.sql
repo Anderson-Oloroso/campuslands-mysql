3793caaf275e9fdebcbdea51bddf2c3e0343dcc9
perezcleidy069-sketch <perezcleidy069@gmail.com>
2026-08-17 08:53:32 -0600

    feat(intermedio): Crear la tabla de Artistas

--- basico/ejercicio-021/resoluciones/cleidy-perez/ddl/schema.sql
+++ basico/ejercicio-021/resoluciones/cleidy-perez/ddl/schema.sql
@@ -4,7 +4,7 @@
 CREATE DATABASE IF NOT EXISTS dibujo_digital;
 USE dibujo_digital;
 -- 1. Tabla de Ilustradores / Artistas
-CREATE TABLE Artistas (
+CREATE TABLE A rtistas (
     artista_id INT PRIMARY KEY AUTO_INCREMENT,
     nombre VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE
