04e67d5c0c71fa687be58a63df743713110fa9d7
perezcleidy069-sketch <perezcleidy069@gmail.com>
2026-08-16 22:35:53 -0600

    feat(intermedio): Crear tabla de saltos

--- intermedio/ejercicio-019/resoluciones/cleidy-perez/ddl/schema.sql
+++ intermedio/ejercicio-019/resoluciones/cleidy-perez/ddl/schema.sql
@@ -16,3 +16,15 @@ CREATE TABLE instructores (
     nombre VARCHAR(100) NOT NULL,
     saltos_certificados INT NOT NULL
 );
+
+CREATE TABLE saltos (
+    salto_id INT AUTO_INCREMENT PRIMARY KEY,
+    paracaidista_id INT NOT NULL,
+    instructor_id INT NOT NULL,
+    altitud_pies INT NOT NULL,
+    tipo_salto VARCHAR(50) NOT NULL, -- 'Tándem', 'Solo AFF', 'Formación'
+    precio DECIMAL(10, 2) NOT NULL,
+    fecha_salto DATE NOT NULL,
+    FOREIGN KEY (paracaidista_id) REFERENCES paracaidistas(paracaidista_id),
+    FOREIGN KEY (instructor_id) REFERENCES instructores(instructor_id)
+);
