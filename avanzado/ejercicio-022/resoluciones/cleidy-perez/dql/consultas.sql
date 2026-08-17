26fa26fb0e462106bb15f23a2f7bc3f2982a16ad
perezcleidy069-sketch <perezcleidy069@gmail.com>
2026-08-17 16:47:37 -0600

    feat(avanzado): Consultar

--- avanzado/ejercicio-022/resoluciones/cleidy-perez/dql/consultas.sql
+++ avanzado/ejercicio-022/resoluciones/cleidy-perez/dql/consultas.sql
@@ -1,2 +1,10 @@
 -- Consultas base. Completa o reemplaza segun el enunciado.
 
+SELECT 
+    e.codigo, 
+    e.titulo, 
+    e.dificultad, 
+    t.nombre AS tematica
+FROM ejercicios e
+INNER JOIN tematicas t ON e.id_tematica = t.id
+WHERE e.codigo = 'EJ-022';
