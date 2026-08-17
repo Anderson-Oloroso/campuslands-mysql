0eba172ee1bb151c4dfa8cb6617c457918203b4e
perezcleidy069-sketch <perezcleidy069@gmail.com>
2026-08-17 16:48:44 -0600

    feat(avanzado): Consultar todas las entregas recibidas

--- avanzado/ejercicio-022/resoluciones/cleidy-perez/dql/consultas.sql
+++ avanzado/ejercicio-022/resoluciones/cleidy-perez/dql/consultas.sql
@@ -1,5 +1,5 @@
 -- Consultas base. Completa o reemplaza segun el enunciado.
-
+USE plattform_animacion;
 SELECT 
     e.codigo, 
     e.titulo, 
@@ -7,4 +7,15 @@ SELECT
     t.nombre AS tematica
 FROM ejercicios e
 INNER JOIN tematicas t ON e.id_tematica = t.id
+WHERE e.codigo = 'EJ-022';
+
+SELECT 
+    e.codigo,
+    e.titulo,
+    ent.nombre_alumno,
+    ent.estado,
+    ent.calificacion,
+    ent.fecha_entrega
+FROM entregas ent
+INNER JOIN ejercicios e ON ent.id_ejercicio = e.id
 WHERE e.codigo = 'EJ-022';
