7bbff405dbb1cb436398547a2890206929798003
perezcleidy069-sketch <perezcleidy069@gmail.com>
2026-08-16 23:02:13 -0600

    feat(intermedio): Consultar para obtner las citas

--- intermedio/ejercicio-020/resoluciones/cleidy-perez/dql/consultas.sql
+++ intermedio/ejercicio-020/resoluciones/cleidy-perez/dql/consultas.sql
@@ -1,9 +1,17 @@
 -- Consultas base. Completa o reemplaza segun el enunciado.
-USE campuslands_mysql;
 
-SELECT * FROM intermedio_ejercicio_020;
+USE estudio_tatuajes;
+-- Obtener las citas cuyo costo es superior al costo promedio de todas las citas del estudio
 
-SELECT categoria, COUNT(*) AS total_registros, AVG(puntaje) AS promedio
-FROM intermedio_ejercicio_020
-GROUP BY categoria
-ORDER BY promedio DESC;
+SELECT 
+    c.cita_id,
+    cl.nombre AS cliente,
+    t.nombre AS tatuador,
+    c.costo
+FROM citas c
+JOIN clientes cl ON c.cliente_id = cl.cliente_id
+JOIN tatuadores t ON c.tatuador_id = t.tatuador_id
+WHERE c.costo > (
+    SELECT AVG(costo) 
+    FROM citas
+);
