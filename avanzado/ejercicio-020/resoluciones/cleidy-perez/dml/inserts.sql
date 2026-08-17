b551994a8ace07965370f797fcc71776000d7d1a
perezcleidy069-sketch <perezcleidy069@gmail.com>
2026-08-16 22:54:01 -0600

    feat(avanzado): Insertar datos en la tabla citas

--- avanzado/ejercicio-020/resoluciones/cleidy-perez/dml/inserts.sql
+++ avanzado/ejercicio-020/resoluciones/cleidy-perez/dml/inserts.sql
@@ -11,3 +11,10 @@ INSERT INTO tatuadores (nombre, estilo_especialidad) VALUES
 ('Marcos "Kuro" Silva', 'Blackwork'),
 ('Elena Vega', 'Neotradicional'),
 ('Gabriel Soto', 'Tradicional');
+
+INSERT INTO citas (cliente_id, tatuador_id, fecha_cita, estado, costo) VALUES
+(1, 1, '2026-02-10 14:00:00', 'Completada', 250.00),
+(2, 2, '2026-02-12 10:30:00', 'Completada', 180.00),
+(3, 3, '2026-02-20 16:00:00', 'Pendiente', 300.00),
+(4, 1, '2026-02-22 11:00:00', 'Pendiente', 220.00),
+(1, 4, '2026-01-15 15:00:00', 'Cancelada', 150.00);
