03a764ac19acb784b9a1b3f95e678eb6eeb8e29c
perezcleidy069-sketch <perezcleidy069@gmail.com>
2026-08-17 16:46:23 -0600

    feat(avanzado): Insertar datos en la tabla ejercicios

--- avanzado/ejercicio-022/resoluciones/cleidy-perez/dml/inserts.sql
+++ avanzado/ejercicio-022/resoluciones/cleidy-perez/dml/inserts.sql
@@ -5,3 +5,7 @@ USE plattform_animacion;
 INSERT INTO tematicas (nombre, descripcion) VALUES
 ('animacion 3D', 'Principios, flujos de trabajo, cámaras y dinámicas en animación tridimensional.'),
 ('modelado 3D', 'Creación de mallas, topología y escultura digital.');
+
+INSERT INTO ejercicios (codigo, titulo, dificultad, id_tematica, instrucciones) VALUES
+('EJ-021', 'Principios de Squash & Stretch', 'Intermedio', 1, 'Aplicar deformación de volumen a una esfera durante una caída.'),
+('EJ-022', 'vistas avanzadas para animacion 3D', 'Avanzado aplicado', 1, 'Configurar un sistema de cámaras personalizadas, motion trails y visores multienfoque para optimizar el flujo de trabajo.');
