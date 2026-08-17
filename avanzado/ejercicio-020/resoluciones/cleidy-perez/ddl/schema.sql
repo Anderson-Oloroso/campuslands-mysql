b73b2a179cbff16a0307cc23d3fe5e189883d47c
perezcleidy069-sketch <perezcleidy069@gmail.com>
2026-08-16 22:52:18 -0600

    feat(avanzado): Creat tabla de citas

--- avanzado/ejercicio-020/resoluciones/cleidy-perez/ddl/schema.sql
+++ avanzado/ejercicio-020/resoluciones/cleidy-perez/ddl/schema.sql
@@ -20,3 +20,14 @@ CREATE TABLE tatuadores (
     nombre VARCHAR(100) NOT NULL,
     estilo_especialidad VARCHAR(50) NOT NULL -- 'Realismo', 'Tradicional', 'Neotradicional', 'Blackwork'
 );
+
+CREATE TABLE citas (
+    cita_id INT AUTO_INCREMENT PRIMARY KEY,
+    cliente_id INT NOT NULL,
+    tatuador_id INT NOT NULL,
+    fecha_cita DATETIME NOT NULL,
+    estado VARCHAR(30) NOT NULL, -- 'Completada', 'Pendiente', 'Cancelada'
+    costo DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (cliente_id) REFERENCES clientes(cliente_id),
+    FOREIGN KEY (tatuador_id) REFERENCES tatuadores(tatuador_id)
+);
