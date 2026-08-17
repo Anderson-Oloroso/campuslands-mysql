57538243f4c837470b118f7bf8f27e2921a57da4
perezcleidy069-sketch <perezcleidy069@gmail.com>
2026-08-17 09:25:53 -0600

    feat(avanzado): Ingresar datos en la tabla Artistas

--- avanzado/ejercicio-021/resoluciones/cleidy-perez/dml/inserts.sql
+++ avanzado/ejercicio-021/resoluciones/cleidy-perez/dml/inserts.sql
@@ -1,1 +1,6 @@
 -- Datos de practica. Puedes cambiarlos o agregar mas filas.
+USE dibujo_digital;
+
+INSERT INTO Artistas (artista_id, nombre, email) VALUES
+(1, 'Sofía Ramírez', 'sofia.art@example.com'),
+(2, 'Carlos Mendoza', 'cmendoza@example.com');
