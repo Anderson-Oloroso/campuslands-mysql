86be144f77a75871aa833bd7f79f88f58f5879b4
perezcleidy069-sketch <perezcleidy069@gmail.com>
2026-08-16 22:53:22 -0600

    feat(avanzado): Insertar datos en la tabla clientes

--- avanzado/ejercicio-020/resoluciones/cleidy-perez/dml/inserts.sql
+++ avanzado/ejercicio-020/resoluciones/cleidy-perez/dml/inserts.sql
@@ -1,2 +1,7 @@
 -- Datos de practica. Puedes cambiarlos o agregar mas filas.
-
+USE estudio_tatuajes;
+INSERT INTO clientes (nombre, email, telefono) VALUES
+('Valeria Ríos', 'valeria.rios@email.com', '+502 5555-0101'),
+('Mateo Fernández', 'mateo.f@email.com', '+502 5555-0102'),
+('Camila Morales', 'camila.m@email.com', '+502 5555-0103'),
+('Daniel Ortiz', 'daniel.ortiz@email.com', '+502 5555-0104');
