95ac63ac0c4e18821fdb15af235bddebbf8a67f6
perezcleidy069-sketch <perezcleidy069@gmail.com>
2026-08-16 22:22:20 -0600

    feat(intermedio): crear la tabla de reservas

--- intermedio/ejercicio-018/resoluciones/cleidy-perez/ddl/schema.sql
+++ intermedio/ejercicio-018/resoluciones/cleidy-perez/ddl/schema.sql
@@ -17,3 +17,14 @@ CREATE TABLE paquetes_turisticos (
     precio_por_noche DECIMAL(10, 2) NOT NULL,
     descuento_porcentaje DECIMAL(5, 2) DEFAULT 0.00
 );
+
+CREATE TABLE reservas (
+    reserva_id INT AUTO_INCREMENT PRIMARY KEY,
+    turista_id INT NOT NULL,
+    paquete_id INT NOT NULL,
+    fecha_inicio DATE NOT NULL,
+    noches INT NOT NULL,
+    personas INT NOT NULL,
+    FOREIGN KEY (turista_id) REFERENCES turistas(turista_id),
+    FOREIGN KEY (paquete_id) REFERENCES paquetes_turisticos(paquete_id)
+);
