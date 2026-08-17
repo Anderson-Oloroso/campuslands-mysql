011c50ce16d0dd47dd2fb718cb5162f03daa776e
perezcleidy069-sketch <perezcleidy069@gmail.com>
2026-08-16 22:55:59 -0600

    feat(avanzado): Consultar index por fecha

--- avanzado/ejercicio-020/resoluciones/cleidy-perez/dql/consultas.sql
+++ avanzado/ejercicio-020/resoluciones/cleidy-perez/dql/consultas.sql
@@ -6,3 +6,11 @@ ON clientes(email);
 
 CREATE INDEX idx_citas_estado_fecha 
 ON citas(estado, fecha_cita);
+
+EXPLAIN SELECT 
+    cita_id, 
+    fecha_cita, 
+    costo 
+FROM citas 
+WHERE estado = 'Completada' 
+  AND fecha_cita >= '2026-01-01 00:00:00';
