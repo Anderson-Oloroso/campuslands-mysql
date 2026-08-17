a849d523b2af354aea594ad85a43440556ab9d7c
perezcleidy069-sketch <perezcleidy069@gmail.com>
2026-08-17 16:53:56 -0600

    feat(intermedio): Insertar datos en la tabla entregas

--- intermedio/ejercicio-022/resoluciones/cleidy-perez/dml/inserts.sql
+++ intermedio/ejercicio-022/resoluciones/cleidy-perez/dml/inserts.sql
@@ -9,3 +9,7 @@ INSERT INTO tematicas (nombre, descripcion) VALUES
 INSERT INTO ejercicios (codigo, titulo, dificultad, id_tematica, instrucciones) VALUES
 ('EJ-021', 'Principios de Squash & Stretch', 'Intermedio', 1, 'Aplicar deformación de volumen a una esfera durante una caída.'),
 ('EJ-022', 'vistas avanzadas para animacion 3D', 'Avanzado aplicado', 1, 'Configurar un sistema de cámaras personalizadas, motion trails y visores multienfoque para optimizar el flujo de trabajo.');
+
+INSERT INTO entregas (id_ejercicio, nombre_alumno, url_archivo_3d, estado, calificacion) VALUES
+(2, 'Carlos Mendoza', 'https://storage.ejemplo.com/entregas/carlos_ej022.mb', 'Aprobado', 9.50),
+(2, 'Ana Gómez', 'https://storage.ejemplo.com/entregas/ana_ej022.blend', 'En Revisión', NULL);
