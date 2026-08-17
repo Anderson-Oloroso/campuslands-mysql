1e852d7c908bdaaf766f79be468a62947e91b211
perezcleidy069-sketch <perezcleidy069@gmail.com>
2026-08-16 22:07:21 -0600

    feat(avanzado): Crear las tablas

---
+++ avanzado/ejercicio-017/resoluciones/cleidy-perez/ddl/schema.sql
@@ -0,0 +1,36 @@
+-- Campuslands MySQL - avanzado ejercicio 017
+-- Ejecuta este archivo antes de inserts.sql.
+
+CREATE DATABASE IF NOT EXISTS tienda_ropa;
+USE tienda_ropa;
+
+
+DROP TABLE IF EXISTS ventas;
+DROP TABLE IF EXISTS productos;
+DROP TABLE IF EXISTS clientes;
+
+
+CREATE TABLE productos (
+    producto_id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    categoria VARCHAR(50) NOT NULL,
+    precio DECIMAL(10, 2) NOT NULL
+);
+
+
+CREATE TABLE clientes (
+    cliente_id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE
+);
+
+
+CREATE TABLE ventas (
+    venta_id INT AUTO_INCREMENT PRIMARY KEY,
+    cliente_id INT NOT NULL,
+    producto_id INT NOT NULL,
+    fecha_venta DATE NOT NULL,
+    cantidad INT NOT NULL,
+    FOREIGN KEY (cliente_id) REFERENCES clientes(cliente_id),
+    FOREIGN KEY (producto_id) REFERENCES productos(producto_id)
+);
