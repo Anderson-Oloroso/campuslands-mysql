28da94b12153c3bb31e88f22d03e7cb49d7a9b14
perezcleidy069-sketch <perezcleidy069@gmail.com>
2026-08-16 22:59:45 -0600

    feat(intermedio): Insertar datos en la tabla tatuadores

--- intermedio/ejercicio-020/resoluciones/cleidy-perez/dml/inserts.sql
+++ intermedio/ejercicio-020/resoluciones/cleidy-perez/dml/inserts.sql
@@ -6,3 +6,8 @@ INSERT INTO clientes (nombre, email, telefono) VALUES
 ('Camila Morales', 'camila.m@email.com', '+502 5555-0103'),
 ('Daniel Ortiz', 'daniel.ortiz@email.com', '+502 5555-0104');
 
+INSERT INTO tatuadores (nombre, estilo_especialidad) VALUES
+('Sofi "Inks" Navarro', 'Realismo'),
+('Marcos "Kuro" Silva', 'Blackwork'),
+('Elena Vega', 'Neotradicional'),
+('Gabriel Soto', 'Tradicional');
