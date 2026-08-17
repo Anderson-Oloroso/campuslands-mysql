b042acc5af3670fca3652c381f72a1370aa1abfa
perezcleidy069-sketch <perezcleidy069@gmail.com>
2026-08-16 22:23:58 -0600

    feat(intermedio): Insertar datos en reservas

--- intermedio/ejercicio-018/resoluciones/cleidy-perez/dml/inserts.sql
+++ intermedio/ejercicio-018/resoluciones/cleidy-perez/dml/inserts.sql
@@ -10,3 +10,8 @@ INSERT INTO paquetes_turisticos (destino, precio_por_noche, descuento_porcentaje
 ('Cancún All Inclusive', 150.00, 10.00),
 ('Tikal y Antigua Guatemala', 120.00, 5.00),
 ('Punta Cana Resort', 200.00, 15.00);
+
+INSERT INTO reservas (turista_id, paquete_id, fecha_inicio, noches, personas) VALUES
+(1, 2, '2026-03-10', 4, 2),
+(2, 1, '2026-04-01', 7, 1),
+(3, 3, '2026-05-15', 5, 3);
