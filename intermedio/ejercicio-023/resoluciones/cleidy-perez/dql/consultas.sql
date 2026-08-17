5e11d844a59d79c3a469a537ba9bd90bb1823c44
perezcleidy069-sketch <perezcleidy069@gmail.com>
2026-08-17 17:11:28 -0600

    feat(intermedio): Consultar lista de estudiantes y calificaciones

--- intermedio/ejercicio-023/resoluciones/cleidy-perez/dql/consultas.sql
+++ intermedio/ejercicio-023/resoluciones/cleidy-perez/dql/consultas.sql
@@ -9,3 +9,15 @@ FROM ejercicios e
 INNER JOIN tematicas t ON e.id_tematica = t.id
 INNER JOIN instructores i ON e.id_instructor = i.id
 WHERE e.codigo = 'EJ-023';
+
+SELECT 
+    e.codigo,
+    est.nombre AS estudiante,
+    ent.estado,
+    ent.calificacion,
+    ent.url_render
+FROM entregas ent
+INNER JOIN ejercicios e ON ent.id_ejercicio = e.id
+INNER JOIN estudiantes est ON ent.id_estudiante = est.id
+WHERE e.codigo = 'EJ-023'
+ORDER BY ent.calificacion DESC;
