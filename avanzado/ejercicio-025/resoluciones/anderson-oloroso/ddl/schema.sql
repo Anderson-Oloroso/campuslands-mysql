8cf02d85a23c8c0e1e4fbb36f316ba54ec1f0d9e
Anderson-Oloroso <henrikog4@gmail.com>
2026-08-18 11:53:31 -0600

    :sparkles: feat(avanzado): Resolver ejercicio 025

---
+++ avanzado/ejercicio-025/resoluciones/anderson-oloroso/ddl/schema.sql
@@ -0,0 +1,27 @@
+USE campuslands_mysql;
+
+CREATE TABLE IF NOT EXISTS formulas_quimicas(
+    formula_id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre_formula VARCHAR(100) NOT NULL,
+    categoria VARCHAR(50) NOT NULL,
+    nivel_riesgo VARCHAR(100) NOT NULL,
+    temperatura_optima DECIMAL(12,2) NOT NULL DEFAULT 0.00,
+    estado VARCHAR(30) NOT NULL DEFAULT 'Activo'
+);
+
+CREATE TABLE IF NOT EXISTS reactivos_quimicos(
+    reactivo_id INT AUTO_INCREMENT PRIMARY KEY,
+    formula_id INT NOT NULL,
+    nombre_reactivo VARCHAR(100) NOT NULL,
+    concentracion_porcentaje VARCHAR(50) NOT NULL,
+    volumen_ml DECIMAL(10,2) NOT NULL DEFAULT 0.00,
+    fecha_registro DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_reactivos_quimicos_formulas_quimicas FOREIGN KEY (formula_id) REFERENCES formulas_quimicas(formula_id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS log_auditoria_formulas_quimicas(
+    log_id INT AUTO_INCREMENT PRIMARY KEY,
+    formula_id INT NOT NULL,
+    accion VARCHAR(50) NOT NULL,
+    fecha DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
