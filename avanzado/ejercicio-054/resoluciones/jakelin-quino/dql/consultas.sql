18769e07d6ba15524fbc1e9d77438688287b8e76
JakelinMishelQuinoMartinez <quinojakelin@gmail.com>
2026-08-18 17:30:21 -0600

    feat(avanzado): resolver ejercicio 054

---
+++ avanzado/ejercicio-054/resoluciones/jakelin-quino/dql/consultas.sql
@@ -0,0 +1,11 @@
+-- 1. Ver proyectos iniciales
+SELECT * FROM `ejercicio-054-adv-proyectos`;
+
+-- 2. Transacción con bloqueo FOR UPDATE
+START TRANSACTION;
+SELECT * FROM `ejercicio-054-adv-proyectos` WHERE id = 1 FOR UPDATE;
+UPDATE `ejercicio-054-adv-proyectos` SET temperatura = 1600 WHERE id = 1;
+COMMIT;
+
+-- 3. Ver proyectos después del bloqueo
+SELECT * FROM `ejercicio-054-adv-proyectos`;
