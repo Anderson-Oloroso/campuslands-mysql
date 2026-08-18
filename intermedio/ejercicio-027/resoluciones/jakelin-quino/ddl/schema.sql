5cf01869417f6c04f10ac5fc318b270e6f9eb34d
JakelinMishelQuinoMartinez <quinojakelin@gmail.com>
2026-08-18 12:30:09 -0600

    feat(intermedio): resolver ejercicio 027

---
+++ intermedio/ejercicio-027/resoluciones/jakelin-quino/ddl/schema.sql
@@ -0,0 +1,10 @@
+-- Ejercicio 027 - Intermedio - Reportes Videojuego Acción y Aventura
+CREATE TABLE `ejercicio-027-int-personajes` (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(50) NOT NULL,
+    tipo VARCHAR(30) NOT NULL,
+    nivel INT DEFAULT 1,
+    vida INT DEFAULT 100,
+    ataque INT DEFAULT 10,
+    misiones_completadas INT DEFAULT 0
+);
