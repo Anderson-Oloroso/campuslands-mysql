cd63994c66deb526c8f7e8422c029a430249b11b
Jose-langL <totherrerajoseluis@gmail.com>
2026-08-18 12:13:22 -0600

    (mysql-intermedio) Ejercicio-033

---
+++ intermedio/ejercicio-033/resoluciones/joseluis-herrera/dml/inserts.sql
@@ -0,0 +1,17 @@
+USE campuslands_mysql;
+
+INSERT INTO inventario_skins
+    (nombre_skin, tipo_arma, rareza, precio, estado)
+VALUES
+    ('Fuego Urbano', 'rifle', 'rara', 35.00, 'disponible'),
+    ('Noche Neon', 'rifle', 'epica', 75.50, 'vendida'),
+    ('Tormenta Azul', 'francotirador', 'legendaria', 150.00, 'disponible'),
+    ('Cobre Tactico', 'pistola', 'comun', 12.50, 'disponible'),
+    ('Dragon Rojo', 'rifle', 'legendaria', 180.00, 'reservada'),
+    ('Hielo Mortal', 'francotirador', 'epica', 95.00, 'disponible'),
+    ('Sombra Digital', 'pistola', 'rara', 28.00, 'vendida'),
+    ('Arena Militar', 'escopeta', 'comun', 18.00, 'disponible'),
+    ('Veneno Verde', 'rifle', 'epica', 82.00, 'disponible'),
+    ('Acero Negro', 'escopeta', 'rara', 42.00, 'reservada'),
+    ('Sol Dorado', 'pistola', 'legendaria', 125.00, 'disponible'),
+    ('Carbono X', 'escopeta', 'epica', 68.00, 'vendida');
