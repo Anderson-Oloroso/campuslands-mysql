b0fdc735592fd467f1cb0c8b80d9ea3b0f204b96
JakelinMishelQuinoMartinez <quinojakelin@gmail.com>
2026-08-18 03:05:19 -0600

    feat(intermedio): resolver ejercicio 017

---
+++ intermedio/ejercicio-017/resoluciones/jakelin-quino/ddl/schema.sql
@@ -0,0 +1,13 @@
+-- Ejercicio 017 - Intermedio - LEFT JOIN Tienda de Ropa
+CREATE TABLE `ejercicio-017-int-categorias` (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE `ejercicio-017-int-productos` (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(50) NOT NULL,
+    categoria_id INT,
+    precio DECIMAL(8,2) DEFAULT 0,
+    FOREIGN KEY (categoria_id) REFERENCES `ejercicio-017-int-categorias`(id)
+);
