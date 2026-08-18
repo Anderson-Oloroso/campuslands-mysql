c0a69dec6f1b2ab62c29634c65ac81d0c42b8f91
JakelinMishelQuinoMartinez <quinojakelin@gmail.com>
2026-08-18 02:47:04 -0600

    feat(intermedio): resolver ejercicio 016

---
+++ intermedio/ejercicio-016/resoluciones/jakelin-quino/dml/inserts.sql
@@ -0,0 +1,8 @@
+INSERT INTO `ejercicio-016-int-categorias` VALUES 
+(1, 'rapida'), (2, 'japonesa'), (3, 'mexicana');
+
+INSERT INTO `ejercicio-016-int-platos` VALUES 
+(1, 'Hamburguesa', 1, 8.50),
+(2, 'Pizza', 1, 12.00),
+(3, 'Sushi', 2, 15.00),
+(4, 'Tacos', 3, 6.00);
