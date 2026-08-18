618366a13aa58f2defcdac489f5584f123035718
Anderson-Oloroso <henrikog4@gmail.com>
2026-08-18 11:46:26 -0600

    :sparkles: feat(avanzado): Resolver ejercicio 021

---
+++ avanzado/ejercicio-021/resoluciones/anderson-oloroso/ddl/schema.sql
@@ -0,0 +1,27 @@
+USE campuslands_mysql;
+
+CREATE TABLE IF NOT EXISTS ilustraciones_digitales(
+    ilustracion_id INT AUTO_INCREMENT PRIMARY KEY,
+    titulo_obra VARCHAR(100) NOT NULL,
+    estilo_arte VARCHAR(50) NOT NULL,
+    software VARCHAR(100) NOT NULL,
+    horas_trabajo DECIMAL(12,2) NOT NULL DEFAULT 0.00,
+    estado VARCHAR(30) NOT NULL DEFAULT 'Activo'
+);
+
+CREATE TABLE IF NOT EXISTS capas_ilustracion(
+    capa_id INT AUTO_INCREMENT PRIMARY KEY,
+    ilustracion_id INT NOT NULL,
+    nombre_capa VARCHAR(100) NOT NULL,
+    opacidad_porcentaje VARCHAR(50) NOT NULL,
+    modo_fusion DECIMAL(10,2) NOT NULL DEFAULT 0.00,
+    fecha_registro DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_capas_ilustracion_ilustraciones_digitales FOREIGN KEY (ilustracion_id) REFERENCES ilustraciones_digitales(ilustracion_id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS log_auditoria_ilustraciones_digitales(
+    log_id INT AUTO_INCREMENT PRIMARY KEY,
+    ilustracion_id INT NOT NULL,
+    accion VARCHAR(50) NOT NULL,
+    fecha DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
