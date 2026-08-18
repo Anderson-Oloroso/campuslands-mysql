6f05c75eb7a641eff323dbdb04072884e1847a10
JakelinMishelQuinoMartinez <quinojakelin@gmail.com>
2026-08-18 12:19:23 -0600

    feat(avanzado): resolver ejercicio 026

---
+++ avanzado/ejercicio-026/resoluciones/jakelin-quino/dml/inserts.sql
@@ -0,0 +1,6 @@
+INSERT INTO `ejercicio-026-adv-personajes` VALUES 
+(1, 'Aragorn', 10, 150, 25, 15),
+(2, 'Gandalf', 15, 120, 30, 10),
+(3, 'Legolas', 8, 110, 28, 12),
+(4, 'Gimli', 12, 180, 20, 20),
+(5, 'Frodo', 5, 80, 10, 5);
