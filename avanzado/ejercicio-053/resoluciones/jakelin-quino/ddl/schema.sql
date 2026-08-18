dd1e3dc80d975b1c95f9a8a79b7d6ba73c1b04b8
JakelinMishelQuinoMartinez <quinojakelin@gmail.com>
2026-08-18 17:28:44 -0600

    feat(avanzado): resolver ejercicio 053

---
+++ avanzado/ejercicio-053/resoluciones/jakelin-quino/ddl/schema.sql
@@ -0,0 +1,13 @@
+-- Ejercicio 053 - Avanzado - Roles y Permisos Arquitectura 3D
+CREATE TABLE `ejercicio-053-adv-edificios` (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(50) NOT NULL,
+    ciudad VARCHAR(30) NOT NULL,
+    altura INT DEFAULT 0
+);
+
+CREATE TABLE `ejercicio-053-adv_usuarios` (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(30) NOT NULL UNIQUE,
+    rol VARCHAR(20) NOT NULL
+);
